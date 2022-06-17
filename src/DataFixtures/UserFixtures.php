9ba5e14f213eb5bb941ee5554f3392374ab61714
Daouda THERA <ddthera@gmail.com>
2022-06-17 01:41:36 +0000

    Initialisation #1

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->hasher = $passwordHasher;
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $user->setName("Daouda S. THERA")
+            ->setEmail("taalralgo@gmail.com")
+            ->setAbout("Je suis developpeur full stack avec plus de 2 ans d'experiences")
+            ->setType("Admin")
+            ->setPassword($this->hasher->hashPassword($user, '123456'))
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}

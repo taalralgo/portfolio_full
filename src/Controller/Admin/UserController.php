4dbed6db988aee9b84ce1f3c8966d7d90a96f260
Daouda THERA <ddthera@gmail.com>
2022-06-19 13:43:47 +0000

    page edit form added sans le form builder

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/users", name="admin_user_index")
+     */
+    public function index(UserRepository $userRepository): Response
+    {
+
+        return $this->render('Admin/user/index.html.twig', [
+            'users' => $userRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/user/edit/{id}", name="admin_user_edit")
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        return $this->render('Admin/user/edit.html.twig', compact('user'));
+    }
+}

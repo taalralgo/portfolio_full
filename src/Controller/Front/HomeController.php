7f9db584298503f3259b897f495465ac05aa02ff
Daouda THERA <ddthera@gmail.com>
2022-06-28 21:43:56 +0000

    header and footer added

---
+++ src/Controller/Front/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller\Front;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="front_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('Front/home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+}

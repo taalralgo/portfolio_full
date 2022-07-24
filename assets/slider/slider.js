21dfa094fec62c52d042f5d25697278132a245e2
Daouda THERA <ddthera@gmail.com>
2022-07-24 20:44:51 +0000

    landing page added successfully

--- assets/slider/slider.js
+++ assets/slider/slider.js
@@ -1,23 +1,19 @@
-// Default theme
-// import '@splidejs/splide/css';
-
-// or other themes
-import Splide from '@splidejs/splide';
-
-import '@splidejs/splide/css/skyblue';
-
-var splide = new Splide( '.splide', {
-    rewind : true,
-    rewindByDrag: true,
-    autoWidth: true,
-    gap: 20,
-    lazyLoad: true,
-    autoplay: true,
-    interval: 3000,
-    rewindSpeed: 1000,
-    speed: 900,
-    arrows: false,
-    pagination: false,
-} );
-
-splide.mount();
+// import Splide from '@splidejs/splide';
+//
+// import '@splidejs/splide/css/skyblue';
+//
+// var splide = new Splide( '.splide', {
+//     rewind : true,
+//     rewindByDrag: true,
+//     autoWidth: true,
+//     gap: 20,
+//     lazyLoad: true,
+//     autoplay: true,
+//     interval: 3000,
+//     rewindSpeed: 1000,
+//     speed: 900,
+//     arrows: false,
+//     pagination: false,
+// } );
+//
+// splide.mount();

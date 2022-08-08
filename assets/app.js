1b5725ba63daca1fb09f3cca5c8b5384d9d2d56b
Daouda THERA <ddthera@gmail.com>
2022-08-08 23:32:28 +0000

    skill section ok

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,22 @@ import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+const next = document.querySelector(".carousel__btn--next"),
+    back = document.querySelector(".carousel__btn--back"),
+    carousel = document.querySelector(".carousel__cards");
+let angle = 0;
+
+next.addEventListener("click", () => {
+    angle -= 45;
+    carousel.style.transform = `translateZ(-25rem) rotateY(${angle}deg)`;
+});
+
+back.addEventListener("click", () => {
+    angle += 45;
+    carousel.style.transform = `translateZ(-25rem) rotateY(${angle}deg)`;
+});
+
+setInterval(function (){
+    next.click();
+}, 4000);

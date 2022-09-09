09483ea305ab50c83431ab0d5ffe1972ed77f812
Daouda THERA <ddthera@gmail.com>
2022-09-09 20:02:31 +0000

    home page animer

--- assets/app.js
+++ assets/app.js
@@ -29,3 +29,51 @@ back.addEventListener("click", () => {
 setInterval(function (){
     next.click();
 }, 4000);
+
+window.onload = function () {
+    var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        autoplay: true,
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+    gsap.from('.landing-header', {
+        duration: 1,
+        opacity: 0,
+        delay: 1,
+        y: '-50%'
+    });
+    gsap.from('.my-photo', {
+        duration: 1,
+        y: '100%',
+        opacity: 0,
+        delay: 1
+    });
+    gsap.from('.study-process', {
+        y: '160',
+        stagger: 0.1,
+        duration: 0.8,
+        ease: 'back'
+    });
+    gsap.to(".geographie", {
+        scrollTrigger: {
+            scrub: 1
+        },
+        scale: 1.5
+    });
+    AOS.init({
+        delay: 10,
+        duration: 1500
+    });
+}
